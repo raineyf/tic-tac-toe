3597f8e54ca98edd0ef61fbcd906581e1e62d0d5
Rainey F <raineyfleming@protonmail.com>
2021-03-26 23:28:33 -0400

    fix dependencies and add logic to check for and display winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,74 +8,127 @@ function Board(props) {
         ["", "", ""],
     ]);
     const [currentPlayer, setCurrentPlayer] = useState("x");
-    const human = props.human;
-    const cpu = props.cpu;
-    const togglePlayer = () => {
-        if (currentPlayer === "x") {
-            setCurrentPlayer("o");
-        } else {
-            setCurrentPlayer("x");
-        }
-    };
-    const spaceAvailable = (y, x) => {
-        return board[y][x] === "";
-    };
-    const availableSpace = () => {
-        const currentBoard = [...board];
-        let availableSpaces = [];
-        for (let y = 0; y < currentBoard.length; y++) {
-            for (let x = 0; x < currentBoard.length; x++) {
-                if (spaceAvailable(y, x)) {
-                    availableSpaces.push({ x: x, y: y });
-                }
-            }
-        }
-        return availableSpaces;
-    };
-    const populateSpace = (y, x, player) => {
-        const newBoard = [...board];
-        newBoard[y][x] = player;
-        setBoard(newBoard);
-    };
-    const cpuMove = () => {
-        const availableSpaces = availableSpace();
-        const randomSpace =
-            availableSpaces[Math.floor(Math.random() * availableSpaces.length)];
-        if (randomSpace) {
-            populateSpace(randomSpace.y, randomSpace.x, cpu);
-            togglePlayer();
-        }
-    };
+    const [gameResult, setGameResult] = useState("");
     const handleSpaceClick = (e) => {
-        const y = e.target.getAttribute("y");
-        const x = e.target.getAttribute("x");
-        populateSpace(y, x, currentPlayer);
-        togglePlayer();
+        const cpu = props.cpu;
+        const human = props.human;
+        if (currentPlayer === human) {
+            const y = e.target.getAttribute("y");
+            const x = e.target.getAttribute("x");
+            const newBoard = [...board];
+            newBoard[y][x] = human;
+            setBoard(newBoard);
+            setCurrentPlayer(cpu);
+        }
+    };
+    const handleReloadClick = () => {
+        window.location.reload();
     };
     useEffect(() => {
-        if (currentPlayer === cpu) {
-            cpuMove();
+        const cpu = props.cpu;
+        const human = props.human;
+        const availableSpace = () => {
+            let availableSpaces = [];
+            for (let y = 0; y < board.length; y++) {
+                for (let x = 0; x < board.length; x++) {
+                    if (board[y][x] === "") {
+                        availableSpaces.push({ x: x, y: y });
+                    }
+                }
+            }
+            return availableSpaces;
+        };
+        const availableSpaces = availableSpace();
+        const checkWinner = () => {
+            let winner = "";
+            for (let y = 0; y < board.length; y++) {
+                if (
+                    board[y][0] === board[y][1] &&
+                    board[y][1] === board[y][2] &&
+                    board[y][0] !== ""
+                ) {
+                    winner = board[y][0];
+                }
+            }
+            for (let x = 0; x < board.length; x++) {
+                if (
+                    board[0][x] === board[1][x] &&
+                    board[1][x] === board[2][x] &&
+                    board[0][x] !== ""
+                ) {
+                    winner = board[0][x];
+                }
+            }
+            if (
+                board[0][0] === board[1][1] &&
+                board[1][1] === board[2][2] &&
+                board[0][0] !== ""
+            ) {
+                winner = board[0][0];
+            }
+            if (
+                board[2][0] === board[1][1] &&
+                board[1][1] === board[0][2] &&
+                board[2][0] !== ""
+            ) {
+                winner = board[2][0];
+            }
+            if (availableSpaces.length === 0 && winner === "") {
+                winner = "Draw";
+            }
+            return winner;
+        };
+        const winner = checkWinner();
+        if (currentPlayer === cpu && winner === "") {
+            const randomSpace =
+                availableSpaces[
+                    Math.floor(Math.random() * availableSpaces.length)
+                ];
+            if (randomSpace) {
+                const newBoard = [...board];
+                newBoard[randomSpace.y][randomSpace.x] = cpu;
+                setBoard(newBoard);
+                setCurrentPlayer(human);
+            }
+        } else if (winner !== "") {
+            setGameResult(winner);
         }
-    }, [currentPlayer]);
+    }, [currentPlayer, board, props.cpu, props.human]);
     return (
-        <div className="board">
-            {board.map((row, y) => {
-                return (
-                    <div className="row" key={y}>
-                        {row.map((space, x) => (
-                            <BoardSpace
-                                key={`${y},${x}`}
-                                x={x}
-                                y={y}
-                                onClick={handleSpaceClick}
-                                value={board[y][x]}
-                                disabled={board[y][x] === "" ? false : true}
-                            />
-                        ))}
-                    </div>
-                );
-            })}
-        </div>
+        <>
+            <div className="board">
+                {board.map((row, y) => {
+                    return (
+                        <div className="row" key={y}>
+                            {row.map((space, x) => (
+                                <BoardSpace
+                                    key={`${y},${x}`}
+                                    x={x}
+                                    y={y}
+                                    onClick={handleSpaceClick}
+                                    value={board[y][x]}
+                                    disabled={board[y][x] === "" ? false : true}
+                                />
+                            ))}
+                        </div>
+                    );
+                })}
+            </div>
+            {gameResult === "Draw" && (
+                <div className="game-result">{gameResult}</div>
+            )}
+            {gameResult === "x" && (
+                <div className="game-result">{gameResult} wins!</div>
+            )}
+            {gameResult === "y" && (
+                <div className="game-result">{gameResult} wins!</div>
+            )}
+            {gameResult !== "" && (
+                <button className="reload-button" onClick={handleReloadClick}>
+                    Refresh
+                </button>
+            )}
+        </>
     );
 }
 
